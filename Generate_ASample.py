7fa1e30e47053b9c3dfef37093ece839b462df1d
chengshen <chengshen@megvii.com>
2019-12-06 16:46:06 +0800

    fixed bugs

--- Generate_ASample.py
+++ Generate_ASample.py
@@ -2,7 +2,11 @@ import torch
 import torch.nn as nn
 from model import tmodels
 from dataset import get_loader
-model = tmodels.resnet50(pretrained=True)
+from torch.autograd import Variable
+import torchvision
+import torch.nn.functional as f
+import numpy as np
+model = tmodels.inception_v3(pretrained=True).cuda()
 print(model)
 
 #model = torch.nn.DataParallel(model).cuda().eval()
@@ -12,43 +16,53 @@ def where(cond, x, y):
 
 dataloader = get_loader()
 
-eps = 30/255
+eps = 32/255
 res = 0
 count = 0
 
 total_num = 0 
-for idx, (xbatch, ybatch, targety_batch) in enumerate(dataloader):
-    #print(torch.max(batch_x), torch.min(batch_x))
+for idx, (xbatch, ybatch, targety_batch, FN) in enumerate(dataloader):
+    name = FN[0]
     batch_x = Variable(xbatch)
+    print(torch.max(batch_x), torch.min(batch_x))
     total_num += batch_x.shape[0]
     batch_y = ybatch
     batch_x = batch_x.cuda()
     print_label = batch_y
+    print_label1 = targety_batch
+    #print(batch_y)
     batch_y = f.one_hot(batch_y, 1000).float().cuda()
+    targety = f.one_hot(targety_batch, 1000).float().cuda()
     #print(batch_x.shape)
     x_adv = Variable(batch_x.data, requires_grad=True)
+    
     #x_adv = hp(x_adv)
-    for i in range(10):
-        h_adv = model(x_adv)
-        cost = torch.mean(torch.sum(batch_y*torch.log(h_adv), dim=1))
-        model.zero_grad()
-        if x_adv.grad is not None:
-            x_adv.grad.data.fill_(0)
+    # for i in range(20):
+    #     h_adv = model(x_adv)
+    #     h_adv = f.sigmoid(h_adv)
+    #     cost1 = 0#torch.mean(torch.sum(batch_y*torch.log(h_adv), dim=1))
+    #     cost2 = -torch.mean(torch.sum(targety*torch.log(h_adv), dim=1))
+    #     cost = cost1 + cost2
+    #     model.zero_grad()
+    #     if x_adv.grad is not None:
+    #         x_adv.grad.data.fill_(0)
 
-        cost.backward()
-        x_adv.grad.sign_()
-        x_adv = x_adv - (2/255) * x_adv.grad
-        x_adv = where(x_adv>batch_x+eps, batch_x+eps, x_adv)
-        x_adv = where(x_adv<batch_x-eps, batch_x-eps, x_adv)
-        x_adv = torch.clamp(x_adv, -1, 1)
-        x_adv = Variable(x_adv.data, requires_grad=True)
+    #     cost.backward()
+    #     x_adv.grad.sign_()
+    #     x_adv = x_adv - (2/255) * x_adv.grad
+    #     x_adv = where(x_adv>batch_x+eps, batch_x+eps, x_adv)
+    #     x_adv = where(x_adv<batch_x-eps, batch_x-eps, x_adv)
+    #     x_adv = torch.clamp(x_adv, -1, 1)
+    #     x_adv = Variable(x_adv.data, requires_grad=True)
     #x_adv = x_adv[:,::-1,:,:]
-    torchvision.utils.save_image(x_adv, "./adv.jpg", normalize=True)
+    #torchvision.utils.save_image(x_adv, "./images/"+name, normalize=True)
     preds = model(x_adv).argmax(dim=1)
     true_list = preds.detach().cpu().numpy() == print_label.numpy()
+    true_list1 = preds.detach().cpu().numpy() == print_label1.numpy()
     res += np.sum(true_list)
     count += 1
-    print(preds, print_label)
+    print(preds, print_label, print_label1)
     print(true_list)
+    print(true_list1)
 
 print(res/total_num)
--- dataset.py
+++ dataset.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn
 
-import torchvision.transforms as transforms
+import torchvision.transforms as T
 from torch.utils.data import Dataset
 from PIL import Image
 import pandas as pd
 import os
 
+import numpy as np
 class Read_Image(Dataset):
     def __init__(self, image_path, csv_file_dir, transforms):
         self.image_path = image_path
@@ -17,21 +18,24 @@ class Read_Image(Dataset):
 
     def __getitem__(self, index):
         filename, true_label, target_label = self.all_data.iloc[index]
-        image = Image.open(os.path.join(self.image_path + filename))
-        return self.transforms(image), torch.FloatTensor(true_label), torch.FloatTensor(target_label)
+        image = Image.open(os.path.join(self.image_path, filename))
+        image = image.convert('RGB')
+        #print(np.array(image).shape)
+        return self.transforms(image), true_label-1, target_label-1, filename
 
     def __len__(self):
         return len(self.all_data)
 
-def get_loader(image_dir='./data/images', csv_file_dir='./data/dev.csv',  batch_size=16):
+def get_loader(image_dir='./data/images', csv_file_dir='./data/dev.csv', mode='test',batch_size=1):
     """Build and return a data loader."""
     transform = []
     if mode == 'train':
         transform.append(T.RandomHorizontalFlip())
-    transform.append(T.CenterCrop(299))
+    #transform.append(T.CenterCrop(299))
     transform.append(T.Resize(299))
     transform.append(T.ToTensor())
-    transform.append(T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)))
+    #transform.append(T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)))
+    transform.append(T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
     transform = T.Compose(transform)
 
 
