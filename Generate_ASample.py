4d2ff849cc87d6c801040efe547a9e61c8eeada9
xuhao02 <xuhao02@megvii.com>
2019-12-06 23:33:12 +0800

    reformate code

--- Generate_ASample.py
+++ Generate_ASample.py
@@ -6,22 +6,26 @@ from torch.autograd import Variable
 import torchvision
 import torch.nn.functional as f
 import numpy as np
+
 model1 = tmodels.inception_v3(pretrained=True).cuda().eval()
 model = tmodels.vgg19(pretrained=True).cuda().eval()
-#print(model)
 
-#model = torch.nn.DataParallel(model).cuda().eval()
+
+# print(model)
+
+# model = torch.nn.DataParallel(model).cuda().eval()
 def where(cond, x, y):
     cond = cond.float()
-    return (cond*x) + ((1-cond)*y)
+    return (cond * x) + ((1 - cond) * y)
+
 
 dataloader = get_loader()
 
-eps = 32/255
+eps = 32 / 255
 res = 0
 count = 0
 
-total_num = 0 
+total_num = 0
 for idx, (xbatch, ybatch, targety_batch, FN) in enumerate(dataloader):
     name = FN[0]
     batch_x = Variable(xbatch)
@@ -31,17 +35,17 @@ for idx, (xbatch, ybatch, targety_batch, FN) in enumerate(dataloader):
     batch_x = batch_x.cuda()
     print_label = batch_y
     print_label1 = targety_batch
-    #print(batch_y)
+    # print(batch_y)
     batch_y = f.one_hot(batch_y, 1000).float().cuda()
     targety = f.one_hot(targety_batch, 1000).float().cuda()
-    #print(batch_x.shape)
+    # print(batch_x.shape)
     x_adv = Variable(batch_x.data, requires_grad=True)
-    
+
     for i in range(20):
         h_adv = model(x_adv)
         h_adv = f.sigmoid(h_adv)
-        cost1 = torch.mean(torch.sum(batch_y*torch.log(h_adv), dim=1))
-        cost2 = -torch.mean(torch.sum(targety*torch.log(h_adv), dim=1))
+        cost1 = torch.mean(torch.sum(batch_y * torch.log(h_adv), dim=1))
+        cost2 = -torch.mean(torch.sum(targety * torch.log(h_adv), dim=1))
         cost = cost1 + cost2
         model.zero_grad()
         if x_adv.grad is not None:
@@ -49,16 +53,16 @@ for idx, (xbatch, ybatch, targety_batch, FN) in enumerate(dataloader):
 
         cost.backward()
         x_adv.grad.sign_()
-        x_adv = x_adv - (2/255) * x_adv.grad
-        x_adv = where(x_adv>batch_x+eps, batch_x+eps, x_adv)
-        x_adv = where(x_adv<batch_x-eps, batch_x-eps, x_adv)
+        x_adv = x_adv - (2 / 255) * x_adv.grad
+        x_adv = where(x_adv > batch_x + eps, batch_x + eps, x_adv)
+        x_adv = where(x_adv < batch_x - eps, batch_x - eps, x_adv)
         x_adv = torch.clamp(x_adv, -1, 1)
         x_adv = Variable(x_adv.data, requires_grad=True)
     for i in range(20):
         h_adv = model1(x_adv)
         h_adv = f.sigmoid(h_adv)
-        cost1 = torch.mean(torch.sum(batch_y*torch.log(h_adv), dim=1))
-        cost2 = -torch.mean(torch.sum(targety*torch.log(h_adv), dim=1))
+        cost1 = torch.mean(torch.sum(batch_y * torch.log(h_adv), dim=1))
+        cost2 = -torch.mean(torch.sum(targety * torch.log(h_adv), dim=1))
         cost = cost1 + cost2
         model1.zero_grad()
         if x_adv.grad is not None:
@@ -66,13 +70,13 @@ for idx, (xbatch, ybatch, targety_batch, FN) in enumerate(dataloader):
 
         cost.backward()
         x_adv.grad.sign_()
-        x_adv = x_adv - (2/255) * x_adv.grad
-        x_adv = where(x_adv>batch_x+eps, batch_x+eps, x_adv)
-        x_adv = where(x_adv<batch_x-eps, batch_x-eps, x_adv)
+        x_adv = x_adv - (2 / 255) * x_adv.grad
+        x_adv = where(x_adv > batch_x + eps, batch_x + eps, x_adv)
+        x_adv = where(x_adv < batch_x - eps, batch_x - eps, x_adv)
         x_adv = torch.clamp(x_adv, -1, 1)
         x_adv = Variable(x_adv.data, requires_grad=True)
-    
-    torchvision.utils.save_image(x_adv, "./images/"+name, normalize=True)
+
+    torchvision.utils.save_image(x_adv, "./images/" + name, normalize=True)
     preds = model1(x_adv).argmax(dim=1)
     true_list = preds.detach().cpu().numpy() == print_label.numpy()
     true_list1 = preds.detach().cpu().numpy() == print_label1.numpy()
@@ -82,4 +86,4 @@ for idx, (xbatch, ybatch, targety_batch, FN) in enumerate(dataloader):
     print(true_list)
     print(true_list1)
 
-print(res/total_num)
+print(res / total_num)
--- dataset.py
+++ dataset.py
@@ -8,6 +8,8 @@ import pandas as pd
 import os
 
 import numpy as np
+
+
 class Read_Image(Dataset):
     def __init__(self, image_path, csv_file_dir, transforms):
         self.image_path = image_path
@@ -20,32 +22,30 @@ class Read_Image(Dataset):
         filename, true_label, target_label = self.all_data.iloc[index]
         image = Image.open(os.path.join(self.image_path, filename))
         image = image.convert('RGB')
-        #print(np.array(image).shape)
-        return self.transforms(image), true_label-1, target_label-1, filename
+        # print(np.array(image).shape)
+        return self.transforms(image), true_label - 1, target_label - 1, filename
 
     def __len__(self):
         return len(self.all_data)
 
-def get_loader(image_dir='./data/images', csv_file_dir='./data/dev.csv', mode='test',batch_size=1):
+
+def get_loader(image_dir='./data/images', csv_file_dir='./data/dev.csv', mode='test', batch_size=1):
     """Build and return a data loader."""
     transform = []
     if mode == 'train':
         transform.append(T.RandomHorizontalFlip())
-    #transform.append(T.CenterCrop(299))
+    # transform.append(T.CenterCrop(299))
     transform.append(T.Resize(299))
     transform.append(T.ToTensor())
-    #transform.append(T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)))
+    # transform.append(T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)))
     transform.append(T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
     transform = T.Compose(transform)
 
-
     dataset = Read_Image(image_dir, csv_file_dir, transform)
 
-
     data_loader = torch.utils.data.DataLoader(dataset=dataset,
-                                  batch_size=batch_size,
-                                  shuffle=False,
-                                  drop_last=False,
-                                  num_workers=1)
+                                              batch_size=batch_size,
+                                              shuffle=False,
+                                              drop_last=False,
+                                              num_workers=1)
     return data_loader
-
